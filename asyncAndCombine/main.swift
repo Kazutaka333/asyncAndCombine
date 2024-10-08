929e597c63c0022101ce2e2eed2348d22e0a67c8
Kazutaka Homma <kaz@flexfitnessapp.com>
2024-10-07 18:18:00 -0700

    first commit

--- asyncAndCombine/main.swift
+++ asyncAndCombine/main.swift
@@ -4,8 +4,31 @@
 //
 //  Created by Kazutaka Homma on 2024-10-07.
 //
+// reference: https://forums.swift.org/t/how-to-mix-async-await-and-combine/49394
 
+import Combine
 import Foundation
 
-print("Hello, World!")
+let subject = PassthroughSubject<Int, Never>()
 
+func handle(_ i: Int) async {
+    print("handling: \(i)")
+    if i >= 100 {
+        exit(0)
+    }
+}
+
+let sub = subject.sink { i in
+//    print("subject1 : \(i)")
+    // this task is not guaranteed to be executed in order
+    Task {
+        await handle(i)
+    }
+}
+
+
+for i in 0...100 {
+    subject.send(i)
+}
+
+RunLoop.main.run()
